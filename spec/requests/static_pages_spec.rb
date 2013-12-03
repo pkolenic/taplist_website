1e3fd6efd5e95f1d19faef41395ea94ca14d7696
Patrick Kolenic <pkolenic@gmail.com>
2013-12-03 14:44:09 -0700

    Remove title duplication in Static Pages Tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "TapList" }
+  
   describe "Home page" do
     it "should have the content 'TapList'" do
       visit '/static_pages/home'
@@ -9,19 +12,31 @@ describe "StaticPages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Taplist | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help" do
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     
-    it "should have hte right title" do
+    it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Taplist | Help')
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    
+    it "should have the right title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
